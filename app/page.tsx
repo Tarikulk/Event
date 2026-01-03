9e346efa2dc872c87bb6b91d94f49b1c483c6a67
Obayed12 <ullahtarik300@gmail.com>
2026-01-03 14:49:21 +0600

    hero created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import ExploreBTN from '@/components/ExploreBTN'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <section>
+    <h1 className='text-center'>The Hub for every dev <br /> You can't miss</h1>
+    <p className='text-center mt-5'>Hackathons, Meetup, and Conference all in one place</p>
+    <ExploreBTN />
+   </section>
   )
 }
 
