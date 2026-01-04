9aa8bf2ffe4d944e23d188ac003161b90e4555a6
Obayed12 <ullahtarik300@gmail.com>
2026-01-04 12:39:44 +0600

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,16 @@ const Home = () => {
     <h1 className='text-center'>The Hub for every dev <br /> You can't miss</h1>
     <p className='text-center mt-5'>Hackathons, Meetup, and Conference all in one place</p>
     <ExploreBTN />
+
+    <div className='mt-20 space-y-7'>
+     <h3>Featured Events</h3>
+
+     <ul className='events'>
+      {[1,2,3,4,5].map((event) => (
+        <li key={event}>Event {event}</li>
+      ))}
+     </ul>
+    </div>
    </section>
   )
 }
